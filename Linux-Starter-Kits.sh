af95b8b102ae50074de3f9dc2f234c5a9133c7a6
lauxjar2 <lauxjar2@gmail.com>
2024-07-02 19:11:24 +0800

    20240702-add Install jupyterlab

--- Linux-Starter-Kits.sh
+++ Linux-Starter-Kits.sh
@@ -158,7 +158,7 @@ elif [ "$1" = "install-jupyterlab" ]; then
     pip install jupyterlab
     pip install  jupyterlab-lsp
     pip install 'python-lsp-server[all]'
-    ln -s / .lsp_symlink
+    sudo ln -s / .lsp_symlink
     # jupyter lab --allow-root --ip <your_LAN_ip> --port 8888
 elif [ "$1" = "install-Ollama-OpenWebUI-AUTOMATIC1111" ]; then
     sudo curl -fsSL https://ollama.com/install.sh | sh
