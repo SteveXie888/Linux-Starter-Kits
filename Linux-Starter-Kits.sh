babf66497eedd7a321936471283439e0a4caedd9
lauxjar2 <lauxjar2@gmail.com>
2024-07-14 13:58:02 +0800

    20240714-OpenWebUI 加入nginx https 反向代理功能。

--- Linux-Starter-Kits.sh
+++ Linux-Starter-Kits.sh
@@ -60,6 +60,104 @@ elif [ "$1" = "install-OpenProject" ]; then
     docker-compose pull
     OPENPROJECT_HTTPS=false docker-compose up -d
 elif [ "$1" = "install-Ollama-OpenWebUI" ]; then
+    sudo yum update -y
+    sudo mkdir /etc/pki/nginx
+    sudo amazon-linux-extras install nginx1 -y
+    sudo openssl genrsa -out /etc/pki/nginx/server.key 2048
+    sudo openssl req -new -x509 -key /etc/pki/nginx/server.key -out /etc/pki/nginx/server.crt -days 3650 -subj "/C=TW/ST=TPI/L=TPI/O=TPI/CN=example.com"
+    sudo bash -c 'cat > /etc/nginx/nginx.conf' << 'EOF'
+# For more information on configuration, see:
+#   * Official English Documentation: http://nginx.org/en/docs/
+#   * Official Russian Documentation: http://nginx.org/ru/docs/
+
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 4096;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen       80;
+        listen       [::]:80;
+        server_name  _;
+        root         /usr/share/nginx/html;
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+        location = /404.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+        }
+    }
+
+# Settings for a TLS enabled server.
+
+    server {
+        listen       443 ssl http2;
+        listen       [::]:443 ssl http2;
+        server_name  _;
+        root         /usr/share/nginx/html;
+
+        ssl_certificate "/etc/pki/nginx/server.crt";
+        ssl_certificate_key "/etc/pki/nginx/server.key";
+        ssl_session_cache shared:SSL:1m;
+        ssl_session_timeout  10m;
+        ssl_ciphers HIGH:!aNULL:!MD5;
+        ssl_prefer_server_ciphers on;
+	location / {
+		proxy_pass http://localhost:8080;  # Forward to localhost:8080
+		proxy_set_header Host $host;
+		proxy_set_header X-Real-IP $remote_addr;
+		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+	}
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+            location = /40x.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+            location = /50x.html {
+        }
+    }
+
+}
+EOF
+    sudo systemctl enable nginx
+    sudo systemctl restart nginx
     # Execute the command to run Ollama installation script
     sudo curl -fsSL https://ollama.com/install.sh | sh
     sudo ollama serve &
@@ -162,6 +260,104 @@ elif [ "$1" = "install-jupyterlab" ]; then
     sudo ln -s / .lsp_symlink
     # jupyter lab --allow-root --ip <your_LAN_ip> --port 8888
 elif [ "$1" = "install-Ollama-OpenWebUI-AUTOMATIC1111" ]; then
+    sudo yum update -y
+    sudo mkdir /etc/pki/nginx
+    sudo amazon-linux-extras install nginx1 -y
+    sudo openssl genrsa -out /etc/pki/nginx/server.key 2048
+    sudo openssl req -new -x509 -key /etc/pki/nginx/server.key -out /etc/pki/nginx/server.crt -days 3650 -subj "/C=TW/ST=TPI/L=TPI/O=TPI/CN=example.com"
+    sudo bash -c 'cat > /etc/nginx/nginx.conf' << 'EOF'
+# For more information on configuration, see:
+#   * Official English Documentation: http://nginx.org/en/docs/
+#   * Official Russian Documentation: http://nginx.org/ru/docs/
+
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 4096;
+
+    include             /etc/nginx/mime.types;
+    default_type        application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+
+    server {
+        listen       80;
+        listen       [::]:80;
+        server_name  _;
+        root         /usr/share/nginx/html;
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+        location = /404.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+        }
+    }
+
+# Settings for a TLS enabled server.
+
+    server {
+        listen       443 ssl http2;
+        listen       [::]:443 ssl http2;
+        server_name  _;
+        root         /usr/share/nginx/html;
+
+        ssl_certificate "/etc/pki/nginx/server.crt";
+        ssl_certificate_key "/etc/pki/nginx/server.key";
+        ssl_session_cache shared:SSL:1m;
+        ssl_session_timeout  10m;
+        ssl_ciphers HIGH:!aNULL:!MD5;
+        ssl_prefer_server_ciphers on;
+	location / {
+		proxy_pass http://localhost:8080;  # Forward to localhost:8080
+		proxy_set_header Host $host;
+		proxy_set_header X-Real-IP $remote_addr;
+		proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+	}
+
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+            location = /40x.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+            location = /50x.html {
+        }
+    }
+
+}
+EOF
+    sudo systemctl enable nginx
+    sudo systemctl restart nginx
     sudo curl -fsSL https://ollama.com/install.sh | sh
     sudo ollama serve &
     sudo docker run -d --network=host --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
